ec3479a913695e60bbcfcb9a5bc1860e0e9da41d
mr33325 <mr33325@gmail.com>
2023-07-23 10:02:29 +0530

    Database table name changed from identity to contact

--- db-BiteSpeed.sql
+++ db-BiteSpeed.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS BiteSpeed;
 
 USE BiteSpeed;
 
-CREATE TABLE identity(
+CREATE TABLE Contact(
     id INT AUTO_INCREMENT PRIMARY KEY,
     phoneNumber VARCHAR(255),
     email VARCHAR(255),
